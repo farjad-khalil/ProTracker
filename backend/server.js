498b28e57321fbd439a4742da0a5f522efccee67
Farjad <farjadkhalil7@gmail.com>
2025-01-31 16:15:20 +0500

    backend | added routes

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+const express  = require('express')
+const app = express()
+
+
+//middleware
+app.use((req,res,next)=>{
+    console.log(req.path,'  ',req.method)
+    next()
+})
+
+
+
+//ROUTES
+app.get('/', (req,res)=>{
+    res.json({mssg:'response for root'})
+})
+//GET all workouts
+app.get('/api/workouts', (req,res)=>{
+    res.json({mssg:'GET all workouts'})
+})
+//GET single workout
+app.get('/api/workouts/:id', (req,res)=>{
+    res.json({mssg:'GET a single workouts'})
+})
+//POST a new workout
+app.post('/api/workouts', (req,res)=>{
+    res.json({mssg:'POST a new workout'})
+})
+//DELETE a workouts
+app.delete('/api/workouts/:id', (req,res)=>{
+    res.json({mssg:'DELETE a single workout'})
+})
+//UPDATE a workouts
+app.patch('/api/workouts/:id', (req,res)=>{
+    res.json({mssg:'UPDATE a workout'})
+})
+
+
+
+
+//listen for req
+app.listen(process.env.PORT , ()=>{
+    console.log('LISTENING TO PORT ', process.env.PORT)
+})
