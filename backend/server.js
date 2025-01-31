0c2e84970ecbaa8ea1b5e3505aef77e06aff7f3a
Farjad <farjadkhalil7@gmail.com>
2025-01-31 17:38:58 +0500

    connected to DB

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,52 @@
 require('dotenv').config()
-const express  = require('express')
+const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 
 //middleware
-app.use((req,res,next)=>{
-    console.log(req.path,'  ',req.method)
+app.use((req, res, next) => {
+    console.log(req.path, '  ', req.method)
     next()
 })
 
 
 
 //ROUTES
-app.get('/', (req,res)=>{
-    res.json({mssg:'response for root'})
+app.get('/', (req, res) => {
+    res.json({ mssg: 'response for root' })
 })
 //GET all workouts
-app.get('/api/workouts', (req,res)=>{
-    res.json({mssg:'GET all workouts'})
+app.get('/api/workouts', (req, res) => {
+    res.json({ mssg: 'GET all workouts' })
 })
 //GET single workout
-app.get('/api/workouts/:id', (req,res)=>{
-    res.json({mssg:'GET a single workouts'})
+app.get('/api/workouts/:id', (req, res) => {
+    res.json({ mssg: 'GET a single workouts' })
 })
 //POST a new workout
-app.post('/api/workouts', (req,res)=>{
-    res.json({mssg:'POST a new workout'})
+app.post('/api/workouts', (req, res) => {
+    res.json({ mssg: 'POST a new workout' })
 })
 //DELETE a workouts
-app.delete('/api/workouts/:id', (req,res)=>{
-    res.json({mssg:'DELETE a single workout'})
+app.delete('/api/workouts/:id', (req, res) => {
+    res.json({ mssg: 'DELETE a single workout' })
 })
 //UPDATE a workouts
-app.patch('/api/workouts/:id', (req,res)=>{
-    res.json({mssg:'UPDATE a workout'})
+app.patch('/api/workouts/:id', (req, res) => {
+    res.json({ mssg: 'UPDATE a workout' })
 })
 
 
 
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log('CONNECTED TO DB & LISTENING TO PORT ', process.env.PORT)
+        })
+    })
+    .catch((err) => {
+        console.log(err);
 
+    })
 //listen for req
-app.listen(process.env.PORT , ()=>{
-    console.log('LISTENING TO PORT ', process.env.PORT)
-})
