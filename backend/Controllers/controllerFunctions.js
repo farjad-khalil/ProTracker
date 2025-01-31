28bf9299da22facbdf75a8ba288adfdf94094b63
Farjad <farjadkhalil7@gmail.com>
2025-01-31 20:35:26 +0500

    added controller fuctions for API endpoints

---
+++ backend/Controllers/controllerFunctions.js
@@ -0,0 +1,73 @@
+const { default: mongoose } = require('mongoose')
+const Workout = require('../Models/workoutModel')
+
+
+//CREATE a new workout
+const createWorkout = async (req,res)=> {
+    const {title,load,reps} =  req.body
+    try{
+        const workout = await Workout.create({title,load,reps})
+        res.status(200).json(workout)
+    }catch(err){
+        res.status(400).json({error:err.message})
+    }
+}
+//GET all workouts
+const getAllWorkout = async (req,res) => {
+    try{
+        const allWorkouts = await Workout.find()
+        res.status(200).json(allWorkouts)
+
+    }catch(err){
+        res.status(400).json({error:err.message})
+    }
+}
+//GET single workout
+const getSinWorkout = async (req,res) => {
+        const {id} = req.params
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(400).json({error:'No such Workout'})
+        }
+        const SinWorkout = await Workout.findById(id)
+        if(!SinWorkout){
+            return res.status(400).json({error:'No such Workout'})
+        }else{
+            res.status(200).json(SinWorkout)
+        }
+
+   
+}
+//DELETE a workout
+const deleteWorkout  = async (req,res)=>{
+    const {id} = req.params
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({error:'No such Workout'})
+    }
+    const workout = await Workout.deleteMany();
+    if(!workout){
+        return res.status(400).json({error:'No such Workout'})
+    }
+    res.status(200).json(workout)
+}
+
+//UPDATE a workout
+const updateWorkout = async (req,res)=>{
+    const {id} = req.params
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({error:'No such Workout'})
+    }
+    const workout = await Workout.findByIdAndUpdate(id,{...req.body} );
+    if(!workout){
+        return res.status(400).json({error:'No such Workout'})
+    }
+    res.status(200).json(workout)
+}
+
+
+module.exports = {
+    createWorkout,
+    getAllWorkout,
+    getSinWorkout,
+    deleteWorkout,
+    updateWorkout
+}
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const {createWorkout,getAllWorkout,getSinWorkout,deleteWorkout,updateWorkout} = require('./Controllers/controllerFunctions')
 const app = express()
 
 
 //middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
     console.log(req.path, '  ', req.method)
     next()
@@ -16,26 +19,24 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
     res.json({ mssg: 'response for root' })
 })
+
+
 //GET all workouts
-app.get('/api/workouts', (req, res) => {
-    res.json({ mssg: 'GET all workouts' })
-})
+app.get('/api/workouts', getAllWorkout)
+
+
 //GET single workout
-app.get('/api/workouts/:id', (req, res) => {
-    res.json({ mssg: 'GET a single workouts' })
-})
+app.get('/api/workouts/:id', getSinWorkout)
+
+
 //POST a new workout
-app.post('/api/workouts', (req, res) => {
-    res.json({ mssg: 'POST a new workout' })
-})
+app.post('/api/workouts', createWorkout)
+
+
 //DELETE a workouts
-app.delete('/api/workouts/:id', (req, res) => {
-    res.json({ mssg: 'DELETE a single workout' })
-})
+app.delete('/api/workouts/:id', deleteWorkout)
 //UPDATE a workouts
-app.patch('/api/workouts/:id', (req, res) => {
-    res.json({ mssg: 'UPDATE a workout' })
-})
+app.patch('/api/workouts/:id', updateWorkout)
 
 
 
